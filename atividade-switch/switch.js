0509eed59ff4284f7096380c93e23d9cdd0a948e
jg4252698-tech <jg4252698@gmail.com>
2026-05-27 11:38:00 -0300

    pasta-html

---
+++ atividade-switch/switch.js
@@ -0,0 +1,10 @@
+function menumatricula() {
+
+    let opcao = document.getElementById("opcoes").value;
+
+    switch (opcao) {
+
+        case "1":
+
+            document.getElementById("resultadoopcoes").innerHTML = "Opção 1: Matricular-se em um curso";
+}
