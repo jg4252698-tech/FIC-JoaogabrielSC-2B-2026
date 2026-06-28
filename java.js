c2213099d784894ef50feec92783ee6a82d0ee83
João <jg4252698@gmail.com>
2026-06-28 00:57:41 -0300

    conclusao-calculadora

--- java.js
+++ java.js
@@ -1,3 +1,32 @@
+// SCRIPT DA CALCULADORA
+
+function insertToDisplay(data) {
+    document.querySelector("#display").value += data;
+}
+
+function limpar(){
+    document.querySelector("#display").value = "";
+}
+
+function apagar(){
+    const display = document.querySelector("#display");
+    display.value = display.value.slice(0, -1);
+}
+
+function resultado(){
+    const display = document.querySelector("#display");
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
+
+    }
+
+}
+
+//   
+
+
 function adicionar() {
     let nome = document.getElementById("nome").value;
     let idade = Number(document.getElementById("idade").value);
